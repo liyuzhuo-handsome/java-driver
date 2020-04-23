a9452eeda7d583df52fb2fa7dbe1bbf39ad7ff58
tomekl007 <tomasz.lelek@datastax.com>
2020-04-23 15:26:31 +0200

    JAVA-2739 add commands for info about cluster, and retrieving username/password

--- core/src/main/java/com/datastax/oss/driver/kubernetesTest/dseoperator/deployCluster.sh
+++ core/src/main/java/com/datastax/oss/driver/kubernetesTest/dseoperator/deployCluster.sh
@@ -24,3 +24,27 @@ kubectl -n cass-operator exec -it -c cassandra cluster1-dc1-default-sts-0 -- nod
 
 # get pod status
 kubectl -n cass-operator describe pod cluster1-dc1-default-sts-0
+
+# create pod that has a connection to DSE cluster
+kubectl run curl --image=radial/busyboxplus:curl -i --tty
+# on the node do:
+nslookup cluster1-dc1-service.cass-operator
+
+# validate that service is exposed using kubectl internal dns
+#Server:    10.96.0.10
+#Address 1: 10.96.0.10 kube-dns.kube-system.svc.cluster.local
+#
+#Name:      cluster1-dc1-service.cass-operator
+#Address 1: 172.17.0.6 172-17-0-6.cluster1-dc1-all-pods-service.cass-operator.svc.cluster.local
+#Address 2: 172.17.0.5 cluster1-dc1-default-sts-1.cluster1-dc1-service.cass-operator.svc.cluster.local
+
+# use java driver image
+# see https://github.com/riptano/java-driver-docker-application/blob/master/buildDockerApp.sh
+
+
+# get username and password
+kubectl -n cass-operator get secret cluster1-superuser
+kubectl -n cass-operator get secret cluster1-superuser -o yaml
+
+# decode both username and password using
+echo base64_value | base64 -D
